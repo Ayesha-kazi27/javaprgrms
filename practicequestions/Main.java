27acb341fba617bb4aebf1753db6cec0e2a08938
Ayesha-kazi27 <130844040+Ayesha-kazi27@users.noreply.github.com>
2026-04-21 10:00:47 +0530

    l

---
+++ practicequestions/Main.java
@@ -0,0 +1,9 @@
+import mypack.Test;
+
+class Main {
+    public static void main(String args[]) {
+        Test t = new Test();
+        t.display();
+        System.out.println("Result = " + t.add(5, 7));
+    }
+}
