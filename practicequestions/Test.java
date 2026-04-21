d16214e9b04564141c9779e235efae60f4ab8755
Ayesha-kazi27 <130844040+Ayesha-kazi27@users.noreply.github.com>
2026-04-21 09:59:41 +0530

    l

---
+++ practicequestions/Test.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Test {
+
+    public void display() {
+        System.out.println("Hello");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
