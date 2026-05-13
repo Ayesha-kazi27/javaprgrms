acff814d38afa0762123c08d2faac4bfaa0656ee
ayesha-kazi27 <ayeshaafzu07@gmail.com>
2026-05-13 13:19:26 +0530

    haha

--- pyqs/pyqs-25/Q1d.java
+++ pyqs/pyqs-25/Q1d.java
@@ -1,5 +1,20 @@
-package Clg_pract_java.pyqs.pyqs-25;
+import java.util.*;
 
 public class Q1d {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check if its prime or not");
+        int n = sc.nextInt();
+        boolean ifprime = true;
+        for(int i = 2;i<n ;i++){
+            if(n%i==0)
+            ifprime = false;
+        }
+        if(ifprime==true){
+            System.out.println("It is a prime number");
+        }else{
+            System.out.println("It is not a prime number");
+        }
+        sc.close();
+    }
 }
